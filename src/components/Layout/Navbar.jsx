1b414972beb1d8a65ba0ac86269515e4c48c8e3e
Kene <kayyungx@gmail.com>
2023-02-25 13:02:05 +0100

    Add naigation bar styling

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,32 +1,38 @@
 import Image from 'next/image';
 import { useState } from 'react';
+import { Space_Grotesk } from '@next/font/google';
+
+const space = Space_Grotesk({ subsets: ['latin'], weight: ['700'] });
 
 const Navbar = () => {
   const [active, setActive] = useState('Home');
-  console.log(active);
 
   const links = ['Home', 'How it works', 'Our Work', 'Pricing', 'About Us'];
 
   return (
-    <div className="flex justify-between items-center px-16 h-[4rem] bg-white">
+    <div className="flex justify-between items-center px-16 h-[4rem] bg-white shadow-md w-full">
       <div className="flex justify-center items-center">
-        <Image
-          src="/images/company_logo.png"
-          alt="company logo"
-          className="w-[6rem] h-[6rem]"
-          width={90}
-          height={90}
-        />
-        <div className="ml-6 font-mono text-base font-semibold">
+        <div>
+          <a href="" className="flex items-center justify-center">
+            <Image
+              src="/images/company_logo.png"
+              alt="company logo"
+              className="w-[6rem] h-[6rem]"
+              width={90}
+              height={90}
+            />
+            <p className={`${space.className} text-2xl`}>Overboard Media</p>
+          </a>
+        </div>
+        <div className="ml-16 font-sans text-base font-semibold">
           <ul className="flex items-center justify-center text-xs">
             {links.map((link) => (
-              <li
-                className="mr-6 text-black hover:border-b hover:border-violet-600"
-                key={link}
-              >
+              <li className="mr-6 text-black relative py-1" key={link}>
                 <a
                   href={`#${link}`}
-                  className={`${active === link && 'text-blue-500'}`}
+                  className={`${
+                    active === link && 'text-[#3461FF]'
+                  } hover-border-left-to-right`}
                   onClick={() => setActive(link)}
                 >
                   {link}
@@ -37,7 +43,9 @@ const Navbar = () => {
         </div>
       </div>
       <div>
-        <button>Try It Now</button>
+        <button className="bg-gradient-to-r from-indigo-700 to-pink-600 hover:from-indigo-800 hover:to-pink-700 text-white font-normal py-2 px-4 rounded-full font-mono">
+          Contact Us
+        </button>
       </div>
     </div>
   );
