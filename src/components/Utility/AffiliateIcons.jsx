3af582ae3837d07d71e788188061b3438b7d94d6
Kene <kayyungx@gmail.com>
2023-02-28 13:01:38 +0100

    Add pictures

---
+++ src/components/Utility/AffiliateIcons.jsx
@@ -0,0 +1,84 @@
+import Image from 'next/image';
+
+const AffiliatesIcons = () => {
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <Image
+          src="/images/Company-logo (1).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo (2).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo (3).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo (4).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo.png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+      </div>
+      <div className="flex justify-between items-center pt-12">
+        <Image
+          src="/images/Company-logo(5).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo (6).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo (7).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo (8).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+        <Image
+          src="/images/Company-logo (9).png"
+          alt="rocket icon"
+          className=""
+          width={90}
+          height={90}
+        />
+      </div>
+    </>
+  );
+};
+
+export default AffiliatesIcons;
