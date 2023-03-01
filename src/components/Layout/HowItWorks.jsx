912fd6dc7e2529a7371563a690cf3a65f36a39c4
Kene <kayyungx@gmail.com>
2023-03-01 23:24:50 +0100

    Initial commit

---
+++ src/components/Layout/HowItWorks.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function HowItWorks() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h3>How It Works</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ea
+            accusantium? Explicabo quasi sed possimus fugit! Excepturi nulla
+            facere delectus amet odit provident dolores dolorum, vero magnam
+            aliquam tenetur vitae!
+          </p>
+        </div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
